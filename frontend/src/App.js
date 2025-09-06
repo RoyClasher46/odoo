012da0a4ece7b4948e565315a58567b4b7c12122
RoyClasher46 <jenishvariya97@gmail.com>
2025-09-06 13:41:24 +0530

    Add frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          jenish variya
         </a>
       </header>
     </div>
